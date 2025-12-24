4094f77f0aa21a29c223239730067e0ad45ead00
mabdullahibrahim30 <mabdullahibrahim30@gmail.com>
2025-12-23 20:29:34 -0800

    Add files via upload

---
+++ saylani2/java.js
@@ -0,0 +1,11 @@
+var uservalue =prompt('please tell your name sir');
+alert('Hi'+uservalue+'welcome to food court website');
+
+var value = prompt('please place your order');
+
+if (value == "briyani") {
+    alert('order placed');
+}
+else {
+    alert('not available');
+} 
